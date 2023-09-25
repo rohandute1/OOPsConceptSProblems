4f447c9a8be66aa80cdf9ec9164184ebdc4fb78e
Rohan <rohandute1@gmail.com>
2023-09-26 00:58:28 +0530

    [rohan]successfully implimented encapsulation

--- RemapOOP'sConcept/Calculator.cs
+++ RemapOOP'sConcept/Calculator.cs
@@ -8,5 +8,26 @@ namespace RemapOOP_sConcept
 {
     internal class Calculator
     {
+        private int AddIntegers(int a, int b)
+        {
+            return a + b;
+        }
+
+        private double AddDoubles(double a, double b)
+        {
+            return a + b;
+        }
+
+        public void CalculateAndDisplay(int a, int b)
+        {
+            int result = AddIntegers(a, b);
+            Console.WriteLine($"Sum of integers: {result}");
+        }
+
+        public void CalculateAndDisplay(double a, double b)
+        {
+            double result = AddDoubles(a, b);
+            Console.WriteLine($"Sum of doubles: {result}");
+        }
     }
 }
